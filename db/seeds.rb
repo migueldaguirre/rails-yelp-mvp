f3b4b97f4f68e64af2c3565de4079a858d4f4008
Miguel Aguirre <md.aguim@gmail.com>
2018-03-28 15:13:35 -0300

    Database seeded with five fake restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "Glasshouse", address: "Av 13th", phone_number: "567123" , category: 'chinese')
+Restaurant.create(name: "The Maple", address: "Av 25th", phone_number: "890765" , category: 'italian')
+Restaurant.create(name: "The Urban Drum", address: "Av 5th", phone_number: "321456" , category: 'italian')
+Restaurant.create(name: "Paragon", address: "Av 36th", phone_number: "425654" , category: 'french')
+Restaurant.create(name: "Luncheon C.", address: "Av 13", phone_number: "567123" , category: 'belgian')
